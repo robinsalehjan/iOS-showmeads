429671c5de85a68c23594ba64986563dba20a270
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-03-16 19:07:18 +0100

    Inital commit

---
+++ ShowMeAds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShowMeAds
+//
+//  Created by Robin Saleh-Jan on 16/3/2018.
+//  Copyright © 2018 Robin Saleh-Jan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ShowMeAdsTests/ShowMeAdsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShowMeAdsTests.swift
+//  ShowMeAdsTests
+//
+//  Created by Robin Saleh-Jan on 16/3/2018.
+//  Copyright © 2018 Robin Saleh-Jan. All rights reserved.
+//
+
+import XCTest
+@testable import ShowMeAds
+
+class ShowMeAdsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
