a10ac008118564491b1033c18653b3077ce47651
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-04-08 10:37:32 +0200

    Add layout constraints for pull down to refresh view

--- ShowMeAds/Extensions/UIViewController+Spinner.swift
+++ ShowMeAds/Extensions/UIViewController+Spinner.swift
@@ -14,17 +14,27 @@ extension UIViewController {
      */
     class func displaySpinner(onView: UIView) -> UIView {
         let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.translatesAutoresizingMaskIntoConstraints = false
         spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.85)
         
         let activityIndicator = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
         activityIndicator.startAnimating()
-        activityIndicator.center = spinnerView.center
         
         DispatchQueue.main.async {
             spinnerView.addSubview(activityIndicator)
+            NSLayoutConstraint.activate([activityIndicator.heightAnchor.constraint(equalTo: spinnerView.heightAnchor),
+                                         activityIndicator.widthAnchor.constraint(equalTo: spinnerView.widthAnchor),
+                                         activityIndicator.centerXAnchor.constraint(equalTo: spinnerView.centerXAnchor),
+                                         activityIndicator.centerYAnchor.constraint(equalTo: spinnerView.centerYAnchor)])
+            
             onView.addSubview(spinnerView)
+            NSLayoutConstraint.activate([spinnerView.heightAnchor.constraint(equalTo: onView.heightAnchor),
+                                         spinnerView.widthAnchor.constraint(equalTo: onView.widthAnchor),
+                                         spinnerView.topAnchor.constraint(equalTo: onView.topAnchor),
+                                         spinnerView.bottomAnchor.constraint(equalTo: onView.bottomAnchor)])
         }
-        
+
         return spinnerView
     }
     
