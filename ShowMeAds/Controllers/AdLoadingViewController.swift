d499bfeb1510fe2761a37312e61c51106da661bc
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-08-24 11:12:50 +0200

    Add activity indicator while loading

--- ShowMeAds/Controllers/AdLoadingViewController.swift
+++ ShowMeAds/Controllers/AdLoadingViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class AdLoadingViewController: UIViewController {
+    fileprivate lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.color = .black
+        view.hidesWhenStopped = true
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .darkGray
+        view.backgroundColor = .white
+        view.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        activityIndicator.startAnimating()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        activityIndicator.stopAnimating()
     }
     
     override func didReceiveMemoryWarning() {
