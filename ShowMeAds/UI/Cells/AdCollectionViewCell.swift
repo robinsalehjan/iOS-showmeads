e7b82f97f9602c697dd51c8d5d969df067f73f07
Robin Saleh-Jan <robinsalehjan@users.noreply.github.com>
2018-09-03 15:24:26 +0200

    Move UI from XIB to code

--- ShowMeAds/UI/Cells/AdCollectionViewCell.swift
+++ ShowMeAds/UI/Cells/AdCollectionViewCell.swift
@@ -34,15 +34,25 @@ class AdCollectionViewCell: UICollectionViewCell {
     fileprivate lazy var priceLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.adjustsFontForContentSizeCategory = true
+        label.backgroundColor = .darkGray
+        label.clipsToBounds = true
         label.layer.cornerRadius = 10
+        label.layer.opacity = 0.80
         label.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMinYCorner]
+        label.font = UIFont.scaledFINNFont(fontType: .medium, size: 18)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.adjustsFontForContentSizeCategory = true
         return label
     }()
     
     fileprivate lazy var locationLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.font = UIFont.scaledFINNFont(fontType: .medium, size: 14)
+        label.textColor = .gray
+        label.textAlignment = .left
         label.adjustsFontForContentSizeCategory = true
         return label
     }()
@@ -50,6 +60,10 @@ class AdCollectionViewCell: UICollectionViewCell {
     fileprivate lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.font = UIFont.scaledFINNFont(fontType: .regular, size: 18)
+        label.textColor = .black
+        label.textAlignment = .left
         label.adjustsFontForContentSizeCategory = true
         return label
     }()
@@ -65,14 +79,34 @@ class AdCollectionViewCell: UICollectionViewCell {
         return button
     }()
     
-    // MARK: - Property injection
+    fileprivate lazy var contentContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        return view
+    }()
     
-    public var imageCache: AdImageCacheService?
+    fileprivate lazy var titleContainerView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    // MARK: - Property injection
     
     public var model: AdItem? {
         didSet {
             guard let model = model else { return }
-            loadImage(imageUrl: model.imageUrl)
+            
+            model.loadImage(imageUrl: model.imageUrl) { (imageData) in
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage.init(data: imageData)
+                }
+            }
+            
             priceLabel.text = (model.price == 0) ?  "Gis bort" : "\(model.price),-"
             locationLabel.text = model.location
             titleLabel.text =  model.title
@@ -83,6 +117,35 @@ class AdCollectionViewCell: UICollectionViewCell {
     // MARK: - Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        contentContainerView.addSubview(imageView)
+        contentContainerView.addSubview(priceLabel)
+        contentContainerView.addSubview(heartButton)
+        
+        titleContainerView.addArrangedSubview(locationLabel)
+        titleContainerView.addArrangedSubview(titleLabel)
+        
+        addSubview(contentContainerView)
+        addSubview(titleContainerView)
+        
+        NSLayoutConstraint.activate([
+            contentContainerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentContainerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentContainerView.topAnchor.constraint(equalTo: topAnchor),
+            contentContainerView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.75),
+            
+            priceLabel.leadingAnchor.constraint(equalTo: contentContainerView.leadingAnchor),
+            priceLabel.trailingAnchor.constraint(equalTo: contentContainerView.centerXAnchor),
+            priceLabel.heightAnchor.constraint(equalTo: contentContainerView.heightAnchor, multiplier: 0.2),
+            priceLabel.bottomAnchor.constraint(equalTo: contentContainerView.bottomAnchor),
+            
+            heartButton.trailingAnchor.constraint(equalTo: contentContainerView.trailingAnchor, constant: -.mediumSpacing),
+            heartButton.topAnchor.constraint(equalTo: contentContainerView.topAnchor, constant: .mediumSpacing),
+            
+            titleContainerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            titleContainerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            titleContainerView.topAnchor.constraint(equalTo: contentContainerView.bottomAnchor, constant: .smallSpacing),
+        ])
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -94,38 +157,21 @@ class AdCollectionViewCell: UICollectionViewCell {
 
 extension AdCollectionViewCell {
     @objc func didTapHeartButton(sender: UIButton) {
-        UIView.animate(withDuration: 0.1, animations: {
+        guard let ad = model else { return }
+        
+        UIView.animate(withDuration: 0.1, animations: { [unowned self] in
             sender.transform = CGAffineTransform(scaleX: 0.90, y: 0.90)
+            
+            switch self.heartButton.isSelected {
+            case true:
+                self.heartButton.isSelected = false
+                self.delegate?.didUnfavorite(ad: ad)
+            case false:
+                self.heartButton.isSelected = true
+                self.delegate?.didFavorite(ad: ad)
+            }
         }) { (didAnimate) in
             sender.transform = CGAffineTransform.identity
         }
-        
-        guard let ad = model else { return }
-        
-        switch heartButton.isSelected {
-        case true:
-            heartButton.isSelected = false
-            delegate?.didUnfavorite(ad: ad)
-        case false:
-            heartButton.isSelected = true
-            delegate?.didFavorite(ad: ad)
-        }
-    }
-}
-
-// MARK: - Private methods
-
-extension AdCollectionViewCell {
-    fileprivate func loadImage(imageUrl: String) {
-        guard URL.init(string: imageUrl) != nil else { fatalError("[ERROR]: The \(imageUrl) is of invalid format") }
-        
-        imageCache?.fetch(url: imageUrl, onCompletion: { [unowned self] (data) in
-            let toData = data as Data
-            let image = UIImage.init(data: toData)
-            
-            DispatchQueue.main.async {
-                self.imageView.image = image
-            }
-        })
     }
 }
